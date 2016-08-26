602a3f1e57aaf47836a91bda71f34cc7a9decce3
Marvin Teichmann <marvin.teichmann@googlemail.com>
2016-08-26 00:38:47 -0400

    Reorganize Repo

---
+++ evals/kitti_eval.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Trains, evaluates and saves the model network using a queue."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import numpy as np
+import scipy as scp
+import random
+from seg_utils import seg_utils as seg
+
+import tensorflow as tf
+import time
+
+
+def eval_image(hypes, gt_image, cnn_image):
+    """."""
+    thresh = np.array(range(0, 256))/255.0
+    road_gt = gt_image[:, :, 2] > 0
+    valid_gt = gt_image[:, :, 0] > 0
+
+    FN, FP, posNum, negNum = seg.evalExp(road_gt, cnn_image,
+                                         thresh, validMap=None,
+                                         validArea=valid_gt)
+
+    return FN, FP, posNum, negNum
+
+
+def evaluate(hypes, sess, image_pl, inf_out):
+
+    softmax = inf_out['softmax']
+    data_dir = hypes['dirs']['data_dir']
+    data_file = hypes['data']['val_file']
+    data_file = os.path.join(data_dir, data_file)
+    image_dir = os.path.dirname(data_file)
+
+    thresh = np.array(range(0, 256))/255.0
+    total_fp = np.zeros(thresh.shape)
+    total_fn = np.zeros(thresh.shape)
+    total_posnum = 0
+    total_negnum = 0
+
+    image_list = []
+
+    with open(data_file) as file:
+        for i, datum in enumerate(file):
+                datum = datum.rstrip()
+                image_file, gt_file = datum.split(" ")
+                image_file = os.path.join(image_dir, image_file)
+                gt_file = os.path.join(image_dir, gt_file)
+
+                image = scp.misc.imread(image_file)
+
+                if hypes['jitter']['fix_shape']:
+                    shape = image.shape
+                    image_height = hypes['jitter']['image_height']
+                    image_width = hypes['jitter']['image_width']
+                    assert(image_height >= shape[0])
+                    assert(image_width >= shape[1])
+
+                    offset_x = (image_height - shape[0])//2
+                    offset_y = (image_width - shape[1])//2
+                    new_image = np.zeros([image_height, image_width, 3])
+                    new_image[offset_x:offset_x+shape[0],
+                              offset_y:offset_y+shape[1]] = image
+                    input_image = new_image
+                else:
+                    input_image = image
+
+                shape = input_image.shape
+
+                gt_image = scp.misc.imread(gt_file)
+                feed_dict = {image_pl: input_image}
+
+                output = sess.run([softmax], feed_dict=feed_dict)
+                output_im = output[0][:, 1].reshape(shape[0], shape[1])
+
+                if hypes['jitter']['fix_shape']:
+                    gt_shape = gt_image.shape
+                    output_im = output_im[offset_x:offset_x+gt_shape[0],
+                                          offset_y:offset_y+gt_shape[1]]
+
+                if i % 5 == 0:
+                    ov_image = seg.make_overlay(image, output_im)
+                    name = os.path.basename(image_file)
+                    image_list.append((name, ov_image))
+
+                FN, FP, posNum, negNum = eval_image(hypes, gt_image, output_im)
+
+                total_fp += FP
+                total_fn += FN
+                total_posnum += posNum
+                total_negnum += negNum
+
+    eval_dict = seg.pxEval_maximizeFMeasure(total_posnum, total_negnum,
+                                            total_fn, total_fp,
+                                            thresh=thresh)
+
+    start_time = time.time()
+    for i in xrange(10):
+        sess.run([softmax], feed_dict=feed_dict)
+    dt = (time.time() - start_time)/10
+
+    eval_list = []
+
+    eval_list.append(('MaxF1', 100*eval_dict['MaxF']))
+    eval_list.append(('BestThresh', 100*eval_dict['BestThresh']))
+    eval_list.append(('Average Precision', 100*eval_dict['AvgPrec']))
+    eval_list.append(('Speed (msec)', 1000*dt))
+    eval_list.append(('Speed (fps)', 1/dt))
+
+    return eval_list, image_list
---
+++ optimizer/generic_optimizer.py
@@ -0,0 +1,82 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+import sys
+import tensorflow as tf
+
+
+# def get_learning_rate(hypes, step):
+#    lr = hypes['solver']['learning_rate']
+#    lr_step = hypes['solver']['learning_rate_step']
+#    if lr_step is not None:
+#      adjusted_lr = (lr * 0.5 ** max(0, (step / lr_step) - 2))
+#        return adjusted_lr
+#    else:
+#        return lr
+
+def get_learning_rate(hypes, step):
+    if "learning_rates" not in hypes['solver']:
+        lr = hypes['solver']['learning_rate']
+        lr_step = hypes['solver']['learning_rate_step']
+        if lr_step is not None:
+            adjusted_lr = (lr * 0.5 ** max(0, (step / lr_step) - 2))
+            return adjusted_lr
+        else:
+            return lr
+
+    for i, num in enumerate(hypes['solver']['steps']):
+        if step < num:
+            return hypes['solver']['learning_rates'][i]
+
+
+def training(hypes, loss, global_step, learning_rate):
+    """Sets up the training Ops.
+
+    Creates a summarizer to track the loss over time in TensorBoard.
+
+    Creates an optimizer and applies the gradients to all trainable variables.
+
+    The Op returned by this function is what must be passed to the
+    `sess.run()` call to cause the model to train.
+
+    Args:
+      loss: Loss tensor, from loss().
+      global_step: Integer Variable counting the number of training steps
+        processed.
+      learning_rate: The learning rate to use for gradient descent.
+
+    Returns:
+      train_op: The Op for training.
+    """
+    # Add a scalar summary for the snapshot loss.''
+    sol = hypes["solver"]
+    hypes['tensors'] = {}
+    hypes['tensors']['global_step'] = global_step
+    total_loss = loss['total_loss']
+    with tf.name_scope('training'):
+
+        if sol['opt'] == 'RMS':
+            opt = tf.train.RMSPropOptimizer(learning_rate=learning_rate,
+                                            decay=0.9, epsilon=sol['epsilon'])
+        elif sol['opt'] == 'Adam':
+            opt = tf.train.AdamOptimizer(learning_rate=learning_rate,
+                                         epsilon=sol['epsilon'])
+        elif sol['opt'] == 'SGD':
+            lr = learning_rate
+            opt = tf.train.GradientDescentOptimizer(learning_rate=lr)
+        else:
+            raise ValueError('Unrecognized opt type')
+
+        grads_and_vars = opt.compute_gradients(total_loss)
+
+        if hypes['clip_norm'] > 0:
+            grads, tvars = zip(*grads_and_vars)
+            clip_norm = hypes["clip_norm"]
+            clipped_grads, norm = tf.clip_by_global_norm(grads, clip_norm)
+            grads_and_vars = zip(clipped_grads, tvars)
+
+        train_op = opt.apply_gradients(grads_and_vars, global_step=global_step)
+
+    return train_op
